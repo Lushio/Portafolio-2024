a3d8074b322e34da07d14b7b8e7b660871669040
HappySama <80355978+Benjamin-Espina@users.noreply.github.com>
2024-10-14 14:37:38 -0300

    Update views.py

--- Fase 2/Evidencias Proyecto/aplicacion/aplicacion_dj/views.py
+++ Fase 2/Evidencias Proyecto/aplicacion/aplicacion_dj/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
-def ficha_lista(request):
-    return render(request,"ficha_lista.html")
 
-def servicio_lista(request):
-    return render(request, "servicio_lista.html")
+def home(request):
+    return render(request, 'app/home.html')
+
+def login(request):
+    return render(request, 'app/login.html')
 
 def ficha_personal(request):
-    return render(request,"ficha_personal.html")
+    return render(request='app/ficha_persola.html')
 
 def form_ingreso(request):
-    return render(request,"form_ingreso.html")
+    return render(request='app/form_ingreso.html')
+
+def ficha_lista(request):
+    return render(request='app/ficha_lista.html')
+
+def servicio_lista(request):
+    return render(request='app/servicio_lista.html')
