de4c0425be61e3dedaebd43e0641c071aecd3872
HappySama <80355978+Benjamin-Espina@users.noreply.github.com>
2024-10-14 14:37:56 -0300

    Update urls.py

--- Fase 2/Evidencias Proyecto/aplicacion/aplicacion_dj/urls.py
+++ Fase 2/Evidencias Proyecto/aplicacion/aplicacion_dj/urls.py
@@ -1,11 +1,11 @@
-from django.contrib import admin
 from django.urls import path
-from .views import ficha_lista,servicio_lista, ficha_personal, form_ingreso
+from .views import home, login, ficha_personal, form_ingreso, ficha_lista, servicio_lista
 
 urlpatterns = [
-    path('ficha_lista/',ficha_lista, name="ficha_lista"),
-    path('servicio_lista/', servicio_lista,name="servicio_lista"),
-    path('ficha_personal/',ficha_personal,name="ficha_personal"),
-    path('form_ingreso/',form_ingreso,name="form_ingreso")
-
+    path('', home, name="home"),
+    path('login/', login, name="login"),
+    path('ficha_personal/', ficha_personal, name="ficha_personal"),
+    path('form_ingreso/', form_ingreso, name="form_ingreso"),
+    path('ficha_lista', ficha_lista, name="ficha_lista"),
+    path('servicio_lista', servicio_lista, name="servicio_lista"),
 ]
